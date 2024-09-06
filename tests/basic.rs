8e0824bf0a6d9311dd2d9b1927e8c83507573d8c
Dan Gohman <dev@sunfishcode.online>
2024-09-05 20:28:57 -0700

    Initial commit, ported from rustix.

---
+++ tests/basic.rs
@@ -0,0 +1,33 @@
+use std::os::fd::AsRawFd;
+
+#[test]
+fn test_proc_self() {
+    // Verify that this API works at all.
+    let fd = rustix_linux_procfs::proc_self_fd().unwrap();
+    assert_ne!(fd.as_raw_fd(), 0);
+}
+
+#[test]
+fn test_status_twice() {
+    let fd = rustix_linux_procfs::proc_self_status().unwrap();
+    drop(fd);
+    let fd = rustix_linux_procfs::proc_self_status().unwrap();
+    drop(fd);
+}
+
+#[test]
+fn parallel_self_proc_status() {
+    const THREADS: usize = 3;
+
+    fn self_proc_status() {
+        rustix_linux_procfs::proc_self_status().expect("error getting proc/self/status pid");
+    }
+
+    let mut handles = Vec::with_capacity(THREADS);
+    for _ in 0..THREADS {
+        handles.push(std::thread::spawn(self_proc_status));
+    }
+    for handle in handles.drain(..) {
+        handle.join().expect("thread crashed");
+    }
+}
